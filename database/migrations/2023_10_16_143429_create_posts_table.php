3612519a934192f113a23f353228fba581eecccb
Fernando Parada <fernandoparada18@protonmail.com>
2023-10-18 17:20:33 -0400

    fix: Campos nuevos en algunas tablas de la base datos

--- database/migrations/2023_10_16_143429_create_posts_table.php
+++ database/migrations/2023_10_16_143429_create_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration {
             $table->string('slug');
             $table->text('body');
             $table->string('image_url');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
