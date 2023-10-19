ca7477f47bad9bab864fb5727f033332ade87578
Fernando Parada <fernandoparada18@protonmail.com>
2023-10-18 22:05:49 -0400

    fix: Mejoras en los campos de base de dato

--- database/migrations/2023_10_16_143429_create_posts_table.php
+++ database/migrations/2023_10_16_143429_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('content');
             $table->string('image_url');
             $table->timestamp('published_at')->nullable();
--- database/migrations/2023_10_16_144451_create_comments_table.php
+++ database/migrations/2023_10_16_144451_create_comments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedBigInteger('post_id');
             $table->foreign('post_id')->references('id')->on('posts');
-            $table->text('body');
+            $table->text('content');
             $table->timestamps();
         });
     }
