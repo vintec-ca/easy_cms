1e4636803d0896e7e9d7e2887d3e31ba5e6b15f0
Fernando Parada <fernandoparada18@protonmail.com>
2023-10-19 01:32:19 -0400

    fix: Traduciones de Posts

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -21,62 +21,94 @@ class PostResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-pencil-square';
 
+    public static function getModelLabel(): string
+    {
+        return __('Post');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('Posts');
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\Select::make('author_id')
+                    ->label(__('Author'))
                     ->relationship('author', 'name')
-                    ->required(),
+                    ->required()
+                    ->columnSpan(2),
                 Forms\Components\Select::make('category_id')
+                    ->label(__('Category'))
                     ->relationship('category', 'name')
-                    ->required(),
+                    ->required()
+                    ->columnSpan(2),
+                Forms\Components\DateTimePicker::make('published_at')
+                    ->translateLabel()
+                    ->timezone('America/Caracas')
+                    ->columnSpan(2),
                 Forms\Components\TextInput::make('title')
+                    ->translateLabel()
                     ->required()
                     ->live(onBlur: true)
                     ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                    ->columnSpan(3)
                     ->maxLength(255),
                 Forms\Components\TextInput::make('slug')
                     ->required()
+                    ->unique()
                     ->disabled()
                     ->dehydrated()
+                    ->columnSpan(3)
                     ->maxLength(255),
                 Forms\Components\RichEditor::make('content')
+                    ->translateLabel()
                     ->required()
                     ->maxLength(65535)
                     ->columnSpanFull(),
                 Forms\Components\FileUpload::make('image_url')
+                    ->label(__('Image'))
                     ->image()
                     ->imageEditor()
-                    ->required(),
+                    ->required()
+                    ->columnSpan(3),
                 Forms\Components\Select::make('tags')
+                    ->label(Str::plural(__('Tag')))
                     ->multiple()
-                    ->relationship('tags', 'name'),
-                Forms\Components\DateTimePicker::make('published_at')
-                    ->timezone('America/Caracas'),
-            ]);
+                    ->relationship('tags', 'name')
+                    ->preload()
+                    ->columnSpan(3),
+            ])
+            ->columns(6);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->translateLabel()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('author.name')
+                    ->label(__('Author'))
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('category.name')
+                    ->label(__('Category'))
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('slug')
-                    ->searchable(),
-                Tables\Columns\ImageColumn::make('image_url'),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->translateLabel()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->translateLabel()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->translateLabel()
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
